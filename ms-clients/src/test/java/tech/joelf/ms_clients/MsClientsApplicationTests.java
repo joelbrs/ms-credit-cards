030461525b2675de3a920018336342f39367b321
Joel Filho <joelfilho02812@gmail.com>
2024-08-29 11:45:48 -0300

    chore(ms-clients): init project

---
+++ ms-clients/src/test/java/tech/joelf/ms_clients/MsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.joelf.ms_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
