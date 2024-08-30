dbd4e5a5094776fa0e636bd5d92bc3d84e4a86d5
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 10:12:35 -0300

    feat(ms-credit-assessor): add client resource with feign client

---
+++ ms-credit-assessor/src/main/java/tech/joelf/ms_credit_assessor/resources/ClientResource.java
@@ -0,0 +1,15 @@
+package tech.joelf.ms_credit_assessor.resources;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import tech.joelf.ms_credit_assessor.models.ClientData;
+
+@FeignClient(value = "ms-clients")
+public interface ClientResource {
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<ClientData> findByCpf(@PathVariable String cpf);
+}
--- ms-credit-assessor/src/main/java/tech/joelf/ms_credit_assessor/services/CreditAssessorService.java
+++ ms-credit-assessor/src/main/java/tech/joelf/ms_credit_assessor/services/CreditAssessorService.java
@@ -3,11 +3,19 @@ package tech.joelf.ms_credit_assessor.services;
 import org.springframework.stereotype.Service;
 
 import tech.joelf.ms_credit_assessor.dtos.ClientStatus;
+import tech.joelf.ms_credit_assessor.resources.ClientResource;
 
 @Service
 public class CreditAssessorService {
 
+    private final ClientResource clientResource;
+
+    public CreditAssessorService(ClientResource clientResource) {
+        this.clientResource = clientResource;
+    }
+
     public ClientStatus getClientStatus(String cpf) {
-        return new ClientStatus();
+        var client = clientResource.findByCpf(cpf).getBody();
+        return new ClientStatus(client, null);
     }
 }
