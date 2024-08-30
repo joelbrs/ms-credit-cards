b7c52c4df0a43b1d4babcf82eec1ed8228e74fa7
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 15:18:45 -0300

    feat(ms-card): add emission card subscriber impl

--- ms-card/src/main/java/tech/joelf/ms_card/mqueue/EmissionCardSubscriber.java
+++ ms-card/src/main/java/tech/joelf/ms_card/mqueue/EmissionCardSubscriber.java
@@ -4,11 +4,39 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import tech.joelf.ms_card.models.Card;
+import tech.joelf.ms_card.models.ClientCard;
+import tech.joelf.ms_card.models.EmissionCardData;
+import tech.joelf.ms_card.repositories.CardRepository;
+import tech.joelf.ms_card.repositories.ClientCardRepository;
+
 @Component
 public class EmissionCardSubscriber {
 
+    private final CardRepository cardRepository;
+    private final ClientCardRepository clientCardRepository;
+
+    public EmissionCardSubscriber(CardRepository cardRepository, ClientCardRepository clientCardRepository) {
+        this.cardRepository = cardRepository;
+        this.clientCardRepository = clientCardRepository;
+    }
+
     @RabbitListener(queues = "${mq.queues.cards-emissions}")
     public void receiveSolicitationEmission(@Payload String payload) {
-        System.out.println(payload);
+        var mapper = new ObjectMapper();
+
+        try {
+            EmissionCardData data = mapper.readValue(payload, EmissionCardData.class);
+
+            Card card = cardRepository.findById(data.getIdCard()).orElseThrow(RuntimeException::new);
+            ClientCard clientCard = new ClientCard(null, data.getCpf(), card, data.getLiberatedLimit());
+
+            clientCardRepository.save(clientCard);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
